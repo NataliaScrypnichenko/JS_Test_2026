7e4f4b1c3a2d194bcee85068d9453658ee64ad67
NataliaScrypnichenko <cornanatalia604@gmail.com>
2026-04-04 22:04:25 +0300

    created variables

---
+++ task_1/main.js
@@ -0,0 +1,11 @@
+// <div class="nameValue">
+//             <h3>Test</h3>
+//             <p></p>
+//             <input id="nameValue" name="ifoNameValue" placeholder="Name=Value">
+//             <button id="buttonAdd">Add</button>
+//             <p id="error" class="error"> </p>
+
+ const input = document.getElementById("nameValue");
+ const buttonAdd = document.getElementById("buttonAdd");
+ const errorInfo = document.getElementById("error");
+ const info = document.getElementById("info");
