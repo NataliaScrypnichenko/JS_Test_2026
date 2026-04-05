4a68905250cc89f2b2f3526978305fcfeaad7c1b
NataliaScrypnichenko <cornanatalia604@gmail.com>
2026-04-05 22:15:45 +0300

    added a visual picture and made the Add button active

--- task_1/main.js
+++ task_1/main.js
@@ -1,11 +1,43 @@
-// <div class="nameValue">
-//             <h3>Test</h3>
-//             <p></p>
-//             <input id="nameValue" name="ifoNameValue" placeholder="Name=Value">
-//             <button id="buttonAdd">Add</button>
-//             <p id="error" class="error"> </p>
 
  const input = document.getElementById("nameValue");
  const buttonAdd = document.getElementById("buttonAdd");
  const errorInfo = document.getElementById("error");
- const info = document.getElementById("info");
+ const divInfo = document.getElementById("info");
+
+
+ buttonAdd.onclick =function ()  {
+        //дыстаю інформацію з input
+         let info=input.value.trim();
+         // за допомоги match прописую яка повинна бути інформація,можна писати любою мовою
+         let infoMatch=info.match(/^([\p{L}]+)\s*=\s*([0-9]+)$/u);
+
+          //провіряэмо чи правильно вели інформацію
+         if (infoMatch){
+             // створю параграф де буде інформація з input
+             let p=document.createElement("p");
+             p.innerText =info;
+
+             // створюю checkbox для вибору інформації щоб її видалити
+             let checkbox = document.createElement("input");
+             checkbox.type = "checkbox";
+             checkbox.classList.add("checkbox_info");
+
+             // прописую форму  тексту для заповнення в divInfo
+             p.innerText = infoMatch[1] + infoMatch[2] + '';
+             p.appendChild(checkbox);
+
+             //додаю інформацію divInfo
+             divInfo.appendChild(p);
+
+             // якщо все вірно інформація про помилку не видається
+             errorInfo.classList.add("error_info");
+             errorInfo.innerText="";
+         }else {
+             errorInfo.innerText=" The information written is incorrect!!! Name is letters and Value is numbers.";
+
+            input.innerText = ""
+         };
+         // чиститься  поле input
+         input.value = "";
+
+ };
