15a7404b7d3ad88e372a923817ce738b93015bc8
NataliaScrypnichenko <cornanatalia604@gmail.com>
2026-04-11 20:20:22 +0300

    not finished yet

---
+++ task_2/index/main.js
@@ -0,0 +1,59 @@
+//В index.html
+// 1 отримати масив об'єктів з endpoint`а https://jsonplaceholder.typicode.com/users
+// 2 Вивести id,name всіх user в index.html. Окремий блок для кожного user.
+// 3 Додати кожному блоку кнопку/посилання ,
+// при кліку на яку відбувається перехід  на сторінку user-details.html,
+// котра має детальну інфорацію про об'єкт на який клікнули
+
+//<form action="#" name="form1" >
+//    <div class="divInfo"  i>
+//
+//    </div>
+// </form>
+
+// let form1=document.forms['form1'];
+let divInfoUsers=document.getElementById("divInfoUsers");
+// let buttonInfo=document.getElementById("buttonInfo");
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(users => users.json())
+    .then(users => {
+      console.log(users);
+      for (const user of users) {
+           console.log(user);
+        let div = document.createElement("div");
+        div.classList.add("div_user");
+
+        let h2=document.createElement("h2");
+        h2.classList.add("user_h2");
+        h2.innerText = user.id;
+
+       let p=document.createElement("p");
+       p.classList.add("info_p");
+       p.innerText = user.name;
+
+       let button = document.createElement("button");
+       button.classList.add("button_info");
+       button.innerText = "info";
+
+       button.onclick = function () {
+         ev.preventDefault();
+         fetch('https://jsonplaceholder.typicode.com/users/post'+ user.id )
+         .then(users => users.json())
+         .then(users => {
+             for (const user of users) {
+
+             }
+         })
+         //+ user.id
+       }
+
+       div.append(h2,p, button);
+
+        divInfoUsers.appendChild(div);
+        // body.appendChild(divInfoUsers);
+        // form1.appendChild(divInfoUsers);
+      }
+
+
+    })
