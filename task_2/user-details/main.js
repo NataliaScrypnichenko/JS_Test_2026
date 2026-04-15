7d85bbd162b8d9eb0d0e62528397e75a09785c4e
NataliaScrypnichenko <cornanatalia604@gmail.com>
2026-04-15 21:56:03 +0300

    I haven't finished this yet.

--- task_2/user-details/main.js
+++ task_2/user-details/main.js
@@ -1,13 +1,44 @@
+ let url = new URL(window.location.href);
+ let userId = url.searchParams.get('id');
+
+let blockUser = document.getElementById('blockUser');
+
+
+ fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+ .then(res => res.json())
+ .then(user => {
+     console.log(user)
+
+      let userDiv = document.createElement('div');
+        userDiv.classList.add('userDiv');
+
+     userDiv.innerText = `
+        ID: ${user.id}
+        Name: ${user.name}
+        Username: ${user.username}
+        Email: ${user.email}
+        Address:
+          Street: ${user.address.street}
+          Suite: ${user.address.suite}
+          City: ${user.address.city}
+          Zipcode: ${user.address.zipcode}
+        Geo:
+          Lat: ${user.address.geo.lat}
+          Lng: ${user.address.geo.lng}
+        Phone: ${user.phone}
+        Website: ${user.website}
+        Company:
+          Name: ${user.company.name}
+          CatchPhrase: ${user.company.catchPhrase}
+          BS: ${user.company.bs}
+     `;
 
 
 
+      blockUser.append(userDiv);
 
-const url = new URL(window.location.href);
-const userId = url.searchParams.get("id");
+      document.body.append(blockUser);
 
-fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
-    .then(user => {
-        console.log(user);
+ })
 
-    })
 
